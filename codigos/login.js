058726eb8bb1bcaf7007ef34ed432c5a424e10ac
Erick Freire <erilefr@gmail.com>
2022-08-08 22:32:17 -0300

    Atualização Crud
    Atualização Crud

---
+++ codigos/login.js
@@ -0,0 +1,5 @@
+var botaologin = document.querySelector("#botao-login");
+
+botaologin.addEventListener('click', function(){
+    window.location.href = "login.html";
+});
---
+++ codigos/ver-chocolates.js
@@ -0,0 +1,5 @@
+var verChocolates = document.querySelector("#ver-chocolates");
+
+verChocolates.addEventListener('click', function(){
+    window.location.href = "#personalizado";
+});
