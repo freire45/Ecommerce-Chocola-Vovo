f05b18deaf0dcacad1ba31aff5b41e531944c611
Erick Freire <erilefr@gmail.com>
2022-09-28 17:48:14 -0300

    a
    a

--- codigos/cadastro_produtos.js
+++ codigos/cadastro_produtos.js
@@ -5,8 +5,18 @@ var preco = document.querySelector("#preco_produto").value;
 var descricao = document.querySelector("#descricao_produto").value;
 var botao_cadastro = document.querySelector("#botao_cadastrar");
 
+var secao_produtos = document.querySelector('.produtos');
+
+var secao_produto = document.createElement('section');
+secao_produto.className = ""
+
+
+var imagem_produto = document.createElement('img');
+
 
 botao_cadastro.addEventListener("click", function(){
+
+
     
 
 });
